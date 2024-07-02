c813650c4d9fb9e4521fa54787779517e1d5d17c
Usuario <Usuario@LAPTOP-BQVKGSJI>
2024-07-02 13:03:47 -0600

    Semana4

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     
-    @RequestMapping("/url")
+    @RequestMapping("/")
     public String page(Model model) {
         model.addAttribute("attribute", "value");
         return "index";
